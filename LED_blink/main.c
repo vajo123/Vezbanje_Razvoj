cdbdaec77580c8987d2da2ae04be56ee0f8e910b
vajo simic <vajosimic123@gmail.com>
2022-05-19 16:35:01 +0200

    function ledBlink added

--- LED_blink/main.c
+++ LED_blink/main.c
@@ -8,10 +8,14 @@ int16_t low_time = 300;
 DDRB |= 1 << 5; // PB5 je izlaz
 while (1)
 {
+	ledBlink(high_time,low_time);
+}
+return 0;
+}
+void ledBlink ( int16_t high_time , int16_t low_time )
+{
 PORTB |= 1 << 5; // LED ON
 _delay_ms ( high_time ) ; // Pauza 1 s
 PORTB &= ~(1 << 5) ; // LED OFF
 _delay_ms ( low_time ) ; // Pauza 1 s
-}
-return 0;
 }
