bd9ceaef20c030f346fb4f71d5592805722119a1
vajo simic <vajosimic123@gmail.com>
2022-05-19 16:36:41 +0200

    function ledInit added

--- LED_blink/main.c
+++ LED_blink/main.c
@@ -5,7 +5,7 @@ int16_t main ()
 {
 int16_t high_time = 700;
 int16_t low_time = 300;
-DDRB |= 1 << 5; // PB5 je izlaz
+ledInit();
 while (1)
 {
 PORTB |= 1 << 5; // LED ON
@@ -14,4 +14,7 @@ PORTB &= ~(1 << 5) ; // LED OFF
 _delay_ms ( low_time ) ; // Pauza 1 s
 }
 return 0;
+}void ledInit ()
+{
+DDRB |= 1 << 5; // PB5 je izlaz
 }
